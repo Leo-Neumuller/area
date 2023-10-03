eda4577414379c4c650a4675f081f1f52f82d4bc
Léo <leo.neumuller@epitech.eu>
2023-10-03 16:46:45 +0200

    [+] Bottom Bar done in App

--- App/App.tsx
+++ App/App.tsx
@@ -1,45 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import Home from './src/screens/Home';
-import {createStackNavigator} from '@react-navigation/stack';
-import { BottomBar } from './src/components/BottomBar';
-import { Text, View } from 'react-native';
-
-const Stack = createStackNavigator()
-
-export const Tmp: React.FC = () => {
-  return(
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>test</Text>
-    </View>
-  )
-}
-
-export const Tmp2: React.FC = () => {
-  return(
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>test2</Text>
-    </View>
-  )
-}
+import { Routes } from './src/screens/Routes';
 
 export default function App() {
-  const list: { name: string, navigationComponent: React.ComponentType, bottomBarComponent: React.JSX.Element }[]
-  = [{name: "test", navigationComponent: Tmp, bottomBarComponent: <Tmp />},
-  {name: "test2", navigationComponent: Tmp2, bottomBarComponent: <Tmp2 />}];
-
-
   return (
-    <>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name='Home' component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    <NavigationContainer>
-      <BottomBar list={list} />
-    </NavigationContainer>
-    </>
+    <Routes />
   );
 }
 
---
+++ App/src/screens/Routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Editor } from "./Flux/Editor";
+import { Text, View } from "react-native";
+import { BottomBar } from "../components/BottomBar";
+
+const Stack = createStackNavigator()
+
+export const Routes: React.FC = () => {
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="BottomBar">
+          <Stack.Screen name='BottomBar' component={BottomBar} options={{headerShown: false}} />
+          <Stack.Screen name='FluxEditor' component={Editor} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+}
