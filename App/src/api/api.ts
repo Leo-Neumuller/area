2e2554597802388bf5aba597a8e6250c8eff025f
Léo <leo.neumuller@epitech.eu>
2023-10-20 17:24:01 +0200

    [~] changed dotenv to process env for api in APP

--- App/src/api/api.ts
+++ App/src/api/api.ts
@@ -1,11 +1,9 @@
 
 import EncryptedStorage from 'react-native-encrypted-storage';
-import {API_URL} from "@env"
-
 
 export async function signupPost (data: {[key: string] : string}) {
     console.log("data", data);
-    const res = await fetch(API_URL +  "/user/signup", {
+    const res = await fetch(process.env.API_URL +  "/user/signup", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -22,7 +20,8 @@ export async function signupPost (data: {[key: string] : string}) {
 }
 
 export async function loginPost (data: {[key: string] : string}) {
-    const res = await fetch(API_URL + "/user/login", {
+    console.log(process.env.API_URL)
+    const res = await fetch(process.env.API_URL + "/user/login", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -40,7 +39,7 @@ export async function loginPost (data: {[key: string] : string}) {
 
 export async function loadUserData() {
     const token = await EncryptedStorage.getItem("userToken");
-    const res = await fetch(API_URL + "/user/me", {
+    const res = await fetch(process.env.API_URL + "/user/me", {
         method: "GET",
         headers: {
             "accept": "application/json",
@@ -55,7 +54,7 @@ export async function loadUserData() {
 }
 
 export async function servicesGet (token: string) {
-    const res = await fetch(API_URL + "/services/services", {
+    const res = await fetch(process.env.API_URL + "/services/services", {
         method: "GET",
         headers: {
             "accept" : "application/json",
@@ -71,7 +70,7 @@ export async function servicesGet (token: string) {
 }
 
 export async function servicesAREAGet (token: string, area: string, service: string) {
-    const res = await fetch(API_URL + "/services/" + service + "/" + area, {
+    const res = await fetch(process.env.API_URL + "/services/" + service + "/" + area, {
         method: "GET",
         headers: {
             "accept" : "application/json",
@@ -87,7 +86,7 @@ export async function servicesAREAGet (token: string, area: string, service: str
 }
 
 export async function serviceSchemaGet (token: string, serviceId: string) {
-    const res = await fetch(API_URL + "/services/metadata/" + serviceId, {
+    const res = await fetch(process.env.API_URL + "/services/metadata/" + serviceId, {
         method: "GET",
         headers: {
             "accept" : "application/json",
@@ -103,7 +102,7 @@ export async function serviceSchemaGet (token: string, serviceId: string) {
 }
 
 export async function authorizeUrlGet (token: string, serviceId: string) {
-    const res = await fetch(API_URL + "/services/" + serviceId + "/authorize_url", {
+    const res = await fetch(process.env.API_URL + "/services/" + serviceId + "/authorize_url", {
         method: "GET",
         headers: {
             "accept" : "application/json",
@@ -120,7 +119,7 @@ export async function authorizeUrlGet (token: string, serviceId: string) {
 
 export async function fluxGet() {
     const token = await EncryptedStorage.getItem("userToken");
-    const res = await fetch(API_URL + "/flux/fluxs", {
+    const res = await fetch(process.env.API_URL + "/flux/fluxs", {
         method: "GET",
         headers: {
             "accept" : "application/json",
