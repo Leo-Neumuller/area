a656dece765a255f59a00772e2132a7f5b6ab109
Hugo GALAN <hugo.galan8@gmail.com>
2023-10-26 07:48:57 +0200

    [*] fixing some issue with service name and data format + adding better debugging of api calls

--- Api/src/utils/Helper.py
+++ Api/src/utils/Helper.py
@@ -1,5 +1,8 @@
+import os
+import traceback
 import warnings
 import sys
+from inspect import getframeinfo, stack
 
 
 def inTestMode():
@@ -24,8 +27,21 @@ def warn(message: str):
     :param message: Message
     :return: None
     """
-    if not inTestMode():  # pragma: no cover
-        warnings.warn(message)
+    if not inTestMode():  # pragma: no cover*
+        if os.environ["DEBUG"] == "True":
+            print(f"\x1b[31;20m" + f"ERROR:\x1b[0m {traceback.format_exc()}", end="")
+        for call in range(min(len(stack()), 3), 0, -1):
+            caller = getframeinfo(stack()[call][0])
+            if call == 1:
+                print(f"\x1b[33;20m" + f"WARNING:\x1b[0m ({caller.filename}:{caller.lineno}) {message}")
+            else:
+                print(f"\x1b[33;20m" + f"WARNING:\x1b[0m ({caller.filename}:{caller.lineno})")
+
+
+def info(message: str):
+    if not inTestMode() and os.environ["DEBUG"] == "True":  # pragma: no cover
+        caller = stack()[1][3]
+        print(f"\x1b[32;20m" + f"INFO: ({caller}) {message}" + "\x1b[0m")
 
 
 def DefaultErrorResponse() -> dict:
