5348fef52e544a8b9ed5bc1bfea58753798d9940
Léo <leo.neumuller@epitech.eu>
2023-10-30 13:37:39 +0100

    [+] added dropdown to create multiple actions/reactions

--- App/src/components/Editor/AREAComponent/index.tsx
+++ App/src/components/Editor/AREAComponent/index.tsx
@@ -12,15 +12,20 @@ type Props = {
     inEditor: boolean,
     onPress: (data?: IAREAComponent) => void,
     data: IAREAComponent,
+    customSvg?: React.ReactNode,
 }
 
-export const AREAComponent: React.FC<Props> = ({inEditor, onPress, data}) => {
+export const AREAComponent: React.FC<Props> = ({inEditor, onPress, data, customSvg}) => {
     const Styles = useThemedStyles(styles);
     const Theme = useTheme();
+    const [selected, setSelected] = React.useState(false);
 
     return (
         <View style={inEditor ? (data.type == "action" ? Styles.actionContainer : Styles.reactionContainer) : Styles.editorContainer}>
-            <TouchableOpacity onPress={() => onPress(data)}>
+            <TouchableOpacity onPress={() => {
+                onPress(data)
+                setSelected(!selected)
+            }}>
                 <View style={inEditor ? Styles.editorContentContainer : Styles.contentContainer}>
                     <View style={[Styles.svgBackground, {backgroundColor: (data.type == 'action' ? Theme.colors.Primary : Theme.colors.Black)}]}>
                         <FluxSVG style={[{color: data.type == 'action' ? Theme.colors.Black : Theme.colors.Primary}, Styles.svgDefault]} />
@@ -30,9 +35,12 @@ export const AREAComponent: React.FC<Props> = ({inEditor, onPress, data}) => {
                             {data.default ? (data.type == 'action' ? "Action" : "Reaction") : data.name!}
                         </Text>
                     </View>
-                    {inEditor && <EditSVG style={[Styles.svgEdit, {
+                    {inEditor && customSvg == undefined && <EditSVG style={[Styles.svgEdit, {
                         color: data.type == 'action' ? Theme.colors.White : Theme.colors.Black
                     }]}/>}
+                    {customSvg &&
+                        customSvg
+                    }
                 </View>
             </TouchableOpacity>
         </View>
--- App/src/components/Editor/Board/index.tsx
+++ App/src/components/Editor/Board/index.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef, useState} from "react";
 import Node from "../../../interfaces/NodeInterface";
 import Edge from "../../../interfaces/EdgeInterface";
-import {GestureResponderEvent, StyleSheet, Text, View} from "react-native";
+import {GestureResponderEvent, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import useThemedStyles from "../../../hooks/Theme/useThemedStyle";
 import useTheme from "../../../hooks/Theme/useTheme";
@@ -15,13 +15,16 @@ import IFLUX from "../../../interfaces/IFLUX";
 import EncryptedStorage from "react-native-encrypted-storage";
 import {fluxPost, servicesGet} from "../../../api/api";
 import {IServiceSchema} from "../../../interfaces/IServiceSchema";
+import DropdownSVG from "../../../../assets/dropdown.svg";
+import AddSVG from "../../../../assets/Vector.svg"
+
 
 async function postFlux(flux: IFLUX): Promise<any> {
     const token = await EncryptedStorage.getItem("userToken");
     return await fluxPost(token as string, flux);
 }
 
-function saveFlux(id: number, title: string, actions: [IAREAComponent], reactions: [IAREAComponent]) {
+function saveFlux(id: number, title: string, actions: IAREAComponent[], reactions: IAREAComponent[]) {
     let flux: IFLUX = {
         id: id,
         name: title,
@@ -36,11 +39,11 @@ function saveFlux(id: number, title: string, actions: [IAREAComponent], reaction
                     subServiceId: action.subService?.id,
                     inputDataFromSubServiceId: action.subService?.id,
                     inputsData: action.serviceSchema?.inputsData,
-                    prevPosition: {x: 300, y: 200},
-                    currPosition: {x: 300, y: 200},
+                    prevPosition: {x: 100 + (index * 500), y: 200},
+                    currPosition: {x: 100 + (index * 500), y: 200},
                     type: "Action",
                     inputEdgeIds: [],
-                    outputEdgeIds: ["edge_0_0"],
+                    outputEdgeIds: [],//"edge_0_0"],
                     title: action.name!,
                     img: "prout",
                 }
@@ -55,28 +58,37 @@ function saveFlux(id: number, title: string, actions: [IAREAComponent], reaction
                 subServiceId: reaction.subService?.id,
                 inputDataFromSubServiceId: reaction.subService?.id,
                 inputsData: reaction.serviceSchema?.inputsData,
-                prevPosition: {x: 300, y: 400},
-                currPosition: {x: 300, y: 400},
+                prevPosition: {x: 100 + (index * 500), y: 400},
+                currPosition: {x: 100 + (index * 500), y: 400},
                 type: "Reaction",
-                inputEdgeIds: ["edge_0_0"],
+                inputEdgeIds: [],//"edge_0_0"],
                 outputEdgeIds: [],
                 title: reaction.name!,
                 img: "prout",
             }
         })],
-        edges: [
-            {
-                id: "edge_0_0",
-                nodeStartId: "node_0",
-                nodeEndId: "node_" + actions.length,
-                inputIndex: -1,
-                outputIndex: 0,
-                prevStartPos: {x: 510, y: 318},
-                currStartPos: {x: 510, y: 318},
-                prevEndPos: {x: 510, y: 382},
-                currEndPos: {x: 510, y: 382},
-            }
-        ]
+        edges: []
+    }
+    for (let i = 0; i < actions.length; i++) {
+        for (let a = actions.length; a < (reactions.length + actions.length); a++)
+        {
+            const startpos = {x: flux.nodes[i].currPosition.x + 210 + (-6 * reactions.length) + (24 * (a - actions.length)), y: 318};
+            const endpos = {x: flux.nodes[a].currPosition.x + 210 + (-6 * actions.length) + (24 * i), y: 382};
+
+            flux.edges.push({
+                id: "edge_" + i + "_" + a,
+                nodeStartId: "node_" + i,
+                nodeEndId: "node_" + a,
+                inputIndex: -1 + i,
+                outputIndex: 0 + i,
+                prevStartPos: startpos,
+                currStartPos: startpos,
+                prevEndPos: endpos,
+                currEndPos: endpos,
+            });
+            flux.nodes[i].outputEdgeIds = [...flux.nodes[i].outputEdgeIds, "edge_" + i + "_" + a]
+            flux.nodes[a].inputEdgeIds = [...flux.nodes[a].inputEdgeIds, "edge_" + i + "_" + a]
+        }
     }
     postFlux(flux).then((res) => {
       console.log(res);
@@ -93,85 +105,105 @@ export const Board: React.FC<{title?: string, setTitle?: React.Dispatch<React.Se
     const [selectedArea, setSelectedArea] = useState<IAREAComponent>();
     const [saveSelectedArea, setSaveSelectedArea] = useState<IAREAComponent>();
     const [areaIsDefined, setAreaIsDefined] = useState<{ action: boolean, reaction: boolean }>({action: false, reaction: false});
-    const [action, setAction] = useState<IAREAComponent>({
+    const [actionDropDown, setActionDropDown] = useState<boolean>(false);
+    const [reactionDropDown, setReactionDropDown] = useState<boolean>(false);
+    const [action, setAction] = useState<IAREAComponent[]>([{
         default: true,
         type: "action",
-    });
-    const [reaction, setReaction] = useState<IAREAComponent>({
+    }]);
+    const [reaction, setReaction] = useState<IAREAComponent[]>([{
         default: true,
         type: "reaction",
-    });
+    }]);
     const [fluxId, setFluxId] = useState<number>(0);
+
+    const defaultAction: IAREAComponent = {
+        default: true,
+        type: "action",
+    }
+    const defaultReaction: IAREAComponent = {
+        default: true,
+        type: "reaction",
+    }
 
     useEffect(() => {
         if (flux) {
-            const tmpaction = flux!.nodes.filter((node) => node.type == "Action")[0];
-            const tmpreaction = flux!.nodes.filter((node) => node.type == "Reaction")[0];
+            const arrActions = flux!.nodes.filter((node) => node.type == "Action");
+            const arrReactions = flux!.nodes.filter((node) => node.type == "Reaction");
 
             setFluxId(flux!.id);
             setAreaIsDefined({action: true, reaction: true})
             setTitle!(flux!.name);
-            setAction({
-                default: false,
-                type: "action",
-                name: tmpaction.service,
-                subService: {
-                    name: tmpaction.subService!,
-                    id: tmpaction.subServiceId!,
-                    description: tmpaction.subService!,
-                },
-                serviceSchema: {
-                    inputsData: tmpaction.inputsData as IServiceSchema["inputsData"],
-                    id: tmpaction.subServiceId!,
-                    name: tmpaction.subService!,
-                    description: tmpaction.subService!,
+            // @ts-ignore
+            setAction(arrActions.map((item) => {
+                return {
+                    default: false,
                     type: "action",
-                    // @ts-ignore
-                    outputData: tmpaction.inputsData?.map((input) => {
-                        return {
-                            id: input.id!,
-                            type: input.type!,
-                            value: input.value!,
-                        }
-                    })
+                    name: item.service,
+                    subService: {
+                        name: item.subService!,
+                        id: item.subServiceId!,
+                        description: item.subService!,
+                    },
+                    serviceSchema: {
+                        inputsData: item.inputsData as IServiceSchema["inputsData"],
+                        id: item.subServiceId!,
+                        name: item.subService!,
+                        description: item.subService!,
+                        type: "action",
+                        // @ts-ignore
+                        outputData: item.inputsData?.map((input) => {
+                            return {
+                                id: input.id!,
+                                type: input.type!,
+                                value: input.value!,
+                            }
+                        })
+                    }
                 }
-            })
-            setReaction({
-                default: false,
-                type: "reaction",
-                name: tmpreaction.service,
-                subService: {
-                    name: tmpreaction.subService!,
-                    id: tmpreaction.subServiceId!,
-                    description: tmpreaction.subService!,
-                },
-                serviceSchema: {
-                    inputsData: tmpreaction.inputsData as IServiceSchema["inputsData"],
-                    id: tmpreaction.subServiceId!,
-                    name: tmpreaction.subService!,
-                    description: tmpreaction.subService!,
+            }));
+            // @ts-ignore
+            setReaction(arrReactions.map((item) => {
+                return {
+                    default: false,
                     type: "reaction",
-                    // @ts-ignore
-                    outputData: tmpreaction.inputsData?.map((input) => {
-                        return {
-                            id: input.id!,
-                            type: input.type!,
-                            value: input.value!,
-                        }
-                    })
+                    name: item.service,
+                    subService: {
+                        name: item.subService!,
+                        id: item.subServiceId!,
+                        description: item.subService!,
+                    },
+                    serviceSchema: {
+                        inputsData: item.inputsData as IServiceSchema["inputsData"],
+                        id: item.subServiceId!,
+                        name: item.subService!,
+                        description: item.subService!,
+                        type: "reaction",
+                        // @ts-ignore
+                        outputData: item.inputsData?.map((input) => {
+                            return {
+                                id: input.id!,
+                                type: input.type!,
+                                value: input.value!,
+                            }
+                        })
+                    }
                 }
-            })
+            }))
         }
     }, [flux]);
 
     useEffect(() => {
         if (saveSelectedArea) {
             if (saveSelectedArea.type == "action") {
-                setAction(saveSelectedArea);
-                console.log(action);
+                const index = action.findIndex((item) => item == selectedArea);
+
+                setAction([...action.slice(0, index), saveSelectedArea, ...action.slice(index + 1)]);
                 setAreaIsDefined({...areaIsDefined, action: true});
             } else if (saveSelectedArea.type == "reaction") {
-                setReaction(saveSelectedArea);
+                const index = reaction.findIndex((item) => item == selectedArea);
+
+                setReaction([...reaction.slice(0, index), saveSelectedArea, ...reaction.slice(index + 1)]);
                 setAreaIsDefined({...areaIsDefined, reaction: true});
             }
         }
@@ -180,23 +212,50 @@ export const Board: React.FC<{title?: string, setTitle?: React.Dispatch<React.Se
     return (
         <BottomSheetComponent ref={bottomSheetRef} setOpened={setBottomSheetOpened} opened={bottomSheetOpened} content={<AREABottomSheet currentArea={selectedArea} bottomSheetRef={bottomSheetRef} setSaveSelectedArea={setSaveSelectedArea} />}>
             <View style={Styles.container}>
-                <AREAComponent data={action} inEditor={true} onPress={(data) => {
-                    bottomSheetRef.current?.expand();
-                    setBottomSheetOpened(true);
-                    setSelectedArea(data);
-
-                }} />
+                <AREAComponent data={{...defaultAction, name: "Actions", default: false}} inEditor={true} onPress={(data) => { setActionDropDown(!actionDropDown) }} customSvg={
+                    <DropdownSVG style={{color: Theme.colors.White, transform: [{rotate: actionDropDown ? "180deg" : "0deg"}]}} />
+                } />
+                <ScrollView style={[Styles.dropdownContainer, {backgroundColor: Theme.colors.Black, display: actionDropDown ? "flex" : "none"}]}>
+                    {action.map((item, index) => {
+                        return (
+                            <AREAComponent key={index} data={item} inEditor={true} onPress={(data) => {
+                                bottomSheetRef.current?.expand();
+                                setBottomSheetOpened(true);
+                                setSelectedArea(data);
+                            }} />
+                        )
+                    })}
+                    <TouchableOpacity style={{bottom: 10}} onPress={() => {
+                        setAction([...action, defaultAction])
+                    }}>
+                        <AddSVG style={{alignSelf: "center", marginTop: 10, color: Theme.colors.White}} />
+                    </TouchableOpacity>
+                </ScrollView>
                 <View style={Styles.line} />
-                <AREAComponent data={reaction} inEditor={true} onPress={(data) => {
-                    bottomSheetRef.current?.expand();
-                    setBottomSheetOpened(true);
-                    setSelectedArea(data);
-                }} />
+                <AREAComponent data={{...defaultReaction, name: "Reactions", default: false}} inEditor={true} onPress={(data) => { setReactionDropDown(!reactionDropDown) }} customSvg={
+                    <DropdownSVG style={{color: Theme.colors.Black, transform: [{rotate: reactionDropDown ? "180deg" : "0deg"}]}} />
+                } />
+                <ScrollView style={[Styles.dropdownContainer, {backgroundColor: Theme.colors.Primary, display: reactionDropDown ? "flex" : "none", marginBottom: 40}]}>
+                {reaction.map((item, index) => {
+                    return (
+                        <AREAComponent key={index} data={item} inEditor={true} onPress={(data) => {
+                            bottomSheetRef.current?.expand();
+                            setBottomSheetOpened(true);
+                            setSelectedArea(data);
+                        }} />
+                    )
+                })}
+                <TouchableOpacity style={{bottom: 10}} onPress={() => {
+                    setReaction([...reaction, defaultReaction])
+                }}>
+                    <AddSVG style={{alignSelf: "center", marginTop: 10, color: Theme.colors.Black}} />
+                </TouchableOpacity>
+                </ScrollView>
                 {areaIsDefined.action && areaIsDefined.reaction &&
                     <View style={Styles.saveButton}>
                         <ButtonComponents onPress={
                             () => {
-                                saveFlux(fluxId, title!, [action], [reaction]);
+                                saveFlux(fluxId, title!, action, reaction);
                                 if (goBack) {
                                     goBack!();
                                 }
@@ -214,6 +273,14 @@ const styles = (Theme: ThemeTypeContext) => StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    dropdownContainer: {
+        marginTop: 3,
+        gap: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 20,
+        maxHeight: "25%",
     },
     line: {
         width: 1,
