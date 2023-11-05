1de92e4864039817b6d127f02ed2349e57994274
Hugo GALAN <hugo.galan8@gmail.com>
2023-11-05 16:06:42 +0100

    [+] adding sign in with google theorical working

--- App/src/screens/LoginScreens/HomeLogin/index.tsx
+++ App/src/screens/LoginScreens/HomeLogin/index.tsx
@@ -9,12 +9,33 @@ import ButtonCompenents from "../../../components/ButtonLogin"
 import useTheme from "../../../hooks/Theme/useTheme";
 import {RFValue} from "react-native-responsive-fontsize";
 import {ThemeTypeContext} from "../../../constants/Theme";
-
-
+import {GoogleSignin, statusCodes} from "@react-native-google-signin/google-signin";
 
 export default function HomeLogin({navigation}) {
     const Styles = useThemedStyles(styles);
     const Theme = useTheme();
+
+    const closeAndSignIn = async () => {
+        try {
+
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+
+            // You can now use userInfo to authenticate the user with your backend
+            console.log('Google Sign-In Success', userInfo);
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // User canceled the Google Sign-In
+                console.log('Google Sign-In canceled');
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log('Google Sign-In is in progress');
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log('Play Services are not available');
+            } else {
+                console.log('Error occurred during Google Sign-In', error);
+            }
+        }
+    };
 
 
     return (
@@ -33,6 +54,24 @@ export default function HomeLogin({navigation}) {
 
                 }}>FLUX</Text>
             </View>
+            {/*<View style={{
+                height: "10%",
+                justifyContent: "space-evenly",
+                width: "100%",
+                alignItems: "center"
+
+            }}>
+                <ButtonCompenents
+                    bgColor={Theme.colors.Black}
+                    hei={"80%"}
+                    wid={"70%"}
+                    text={"SIGN IN GOOGLE"}
+                    textColor={Theme.colors.White}
+                    onPress={() => closeAndSignIn()}
+
+                />
+            </View>*/}
+
             <View style={{
                 height: "10%",
                 justifyContent: "space-evenly",
